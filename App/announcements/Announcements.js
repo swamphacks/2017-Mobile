7b4d598978a009b69084db80e6c5876d15d162ae
Nikhil Thota <nt92@ufl.edu>
2016-12-17 18:28:53 +0100

    made tableview for announcements

--- App/announcements/Announcements.js
+++ App/announcements/Announcements.js
@@ -7,54 +7,35 @@ import { List, ListItem, Text, SearchBar } from 'react-native-elements';
 
 const log = () => console.log('this is an example method');
 
-const list1 = [
+const announcements = [
   {
-    title: 'Appointments',
-    icon: 'av-timer'
+    title: 'Lunch',
+    description: 'FOOOOOOOD',
+    time: 'Sun 9:00AM',
+    tag: 'food'
   },
   {
-    title: 'Trips',
-    icon: 'flight-takeoff'
+    title: 'Lunch',
+    description: 'FOOOOOOOD',
+    time: 'Sun 9:00AM',
+    tag: 'food'
   },
   {
-    title: 'Passwords',
-    icon: 'fingerprint'
+    title: 'Lunch',
+    description: 'FOOOOOOOD',
+    time: 'Sun 9:00AM',
+    tag: 'food'
   },
   {
-    title: 'Pitches',
-    icon: 'lightbulb-outline'
-  },
-  {
-    title: 'Updates',
-    icon: 'track-changes'
-  }
-];
-
-const list2 = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-  {
-    name: 'Amanda Martin',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
-    subtitle: 'CEO'
-  },
-  {
-    name: 'Christy Thomas',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
-    subtitle: 'Lead Developer'
-  },
-  {
-    name: 'Melissa Jones',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/nuraika/128.jpg',
-    subtitle: 'CTO'
+    title: 'Lunch',
+    description: 'FOOOOOOOD',
+    time: 'Sun 9:00AM',
+    tag: 'food'
+  },{
+    title: 'Lunch',
+    description: 'FOOOOOOOD',
+    time: 'Sun 9:00AM',
+    tag: 'food'
   }
 ];
 
@@ -63,7 +44,7 @@ class Announcements extends Component {
     super();
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(list1)
+      dataSource: ds.cloneWithRows(announcements)
     };
     this.renderRow = this.renderRow.bind(this);
   }
@@ -73,81 +54,30 @@ class Announcements extends Component {
         key={sectionID}
         onPress={log}
         title={rowData.title}
-        icon={{name: rowData.icon}}
+        subtitle={rowData.description}
+        time={rowData.time}
+        tag={rowData.tag}
       />
     )
   }
   render () {
     return (
-      <ScrollView keyboardShouldPersistTaps style={styles.mainContainer}>
-        <View style={styles.hero}>
-          <Icon color='white' name='sentiment-very-satisfied' size={62} />
-          <Text style={styles.heading}>Searchbar & List</Text>
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            placeholder='Type Here...' />
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            noIcon
-            placeholder='Type Here...' />
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            round
-            placeholder='Type Here...' />
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            lightTheme
-            placeholder='Type Here...' />
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            noIcon
-            lightTheme
-            placeholder='Type Here...' />
-        </View>
-        <View style={{marginTop: 10, marginBottom: 0}}>
-          <SearchBar
-            round
-            lightTheme
-            placeholder='Type Here...' />
-        </View>
+      <ScrollView keyboardShouldPersistTaps style={styles.scrollView}>
         <List>
           <ListView
             renderRow={this.renderRow}
             dataSource={this.state.dataSource}
             />
         </List>
-        <List containerStyle={{marginBottom: 20}}>
-        {
-          list2.map((l, i) => (
-            <ListItem
-              roundAvatar
-              avatar={{ uri: l.avatar_url }}
-              key={i}
-              onPress={log}
-              title={l.name}
-              subtitle={l.subtitle}
-            />
-          ))
-        }
-        </List>
-        <ListItem
-          roundAvatar
-          title='Nader Dabit'
-          avatar={require('../images/avatar1.jpg')}
-         />
       </ScrollView>
     )
   }
 }
 
 styles = StyleSheet.create({
-  mainContainer: {
-    backgroundColor: '#ebedf1'
+  scrollView: {
+    backgroundColor: 'white',
+    marginTop: 65
   },
   container: {
     marginTop: 60
