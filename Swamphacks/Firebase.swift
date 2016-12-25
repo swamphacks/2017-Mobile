d65da754bb66db790d646da39c3eb44bf3c9af64
gonzalonunez <gonzi@tcpmiami.com>
2016-12-25 16:30:03 -0500

    Going to try Firebase wrapper using objcio generics strategy

--- Swamphacks/Firebase.swift
+++ Swamphacks/Firebase.swift
@@ -8,7 +8,45 @@
 
 import Foundation
 
-struct Firebase {
+typealias JSONDictionary = [String: Any]
+
+public enum Result<A> {
+  case success(A)
+  case error(Error)
+}
+
+extension Result {
+  public init(_ value: A?, or error: Error) {
+    if let value = value {
+      self = .success(value)
+    } else {
+      self = .error(error)
+    }
+  }
   
+  public var value: A? {
+    guard case .success(let v) = self else { return nil }
+    return v
+  }
+}
+
+struct FirebaseResource<A> {
+  let path: String
+  let parse: (JSONDictionary) -> A?
+
+  init(path: String, parse: @escaping (JSONDictionary) -> A?) {
+    self.path = path
+    self.parse = parse
+  }
+}
+
+struct Firebase {
+  let url: URL
+  
+  func load<A>(_ resource: FirebaseResource<A>, completion: @escaping (Result<A>) -> Void) {
+    let resourceURL = url.appendingPathComponent(resource.path)
+    
+    
+  }
 
 }
